e55e32940af8320848e2170eb2499b5f5b2f6632
JJ Merelo <jjmerelo@gmail.com>
2016-09-01 19:19:09 +0200

    Making corrections to files

--- datasets/PETRA/blend.pl
+++ datasets/PETRA/blend.pl
@@ -10,12 +10,13 @@ use v5.14;
 
 my $id = shift || "1011";
 
-my %count_dgt = process_file( "DGT_$id.csv");
-my %count_petra = process_file( "PETRA_$id.csv");
+my %count_dgt = process_file( "DGT_$id.csv", $id);
+my %count_petra = process_file( "PETRA_$id.csv", $id);
 
 say "DoW,Mon,Hour,Detected,Real";
 for my $date ( keys %count_dgt ) {
   next if !$count_petra{$date};
+  next if !$count_dgt{$date};
   my ($year,$mon,$day,$hour) = ($date =~ /(\d+)-(\d+)-(\d+)\s+(\d)/);
   my $dt = DateTime->new(year => $year, month => $mon, day => $day );
   say $dt->day_name,",$mon,$hour,$count_petra{$date},$count_dgt{$date}";
@@ -23,11 +24,13 @@ for my $date ( keys %count_dgt ) {
 
 sub process_file {
   my $file= shift;
+  my $id = shift;
   my @content = read_lines( $file );
   my %count;
   for my $c (@content) {
     chomp($c);
     my ($date, $foo, $count) = split(/[,;]/, $c );
+    next if $foo !~ $id;
     $count{$date} = $count;
   }
   return %count;
