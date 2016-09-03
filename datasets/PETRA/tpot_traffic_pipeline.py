b47073905bad309ea37d269e8e0be44569be3907
JJ Merelo <jjmerelo@gmail.com>
2016-09-03 10:17:52 +0200

    Pipeline generated with @rhiever's TPOT

---
+++ datasets/PETRA/tpot_traffic_pipeline.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import VotingClassifier
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import FunctionTransformer
+from xgboost import XGBRegressor
+
+# NOTE: Make sure that the class is labeled 'class' in the data file
+tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('class'), axis=1)
+training_features, testing_features, training_classes, testing_classes = \
+    train_test_split(features, tpot_data['class'], random_state=42)
+
+exported_pipeline = make_pipeline(
+    XGBRegressor(learning_rate=0.7, max_depth=1, min_child_weight=11, n_estimators=500, subsample=0.4)
+)
+
+exported_pipeline.fit(training_features, training_classes)
+results = exported_pipeline.predict(testing_features)
